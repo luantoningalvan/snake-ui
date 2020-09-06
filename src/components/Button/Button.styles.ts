dd176618f600981d0b8cf7d82b95b894399babc5
Luan Tonin Galvan <ltoningalvan@gmail.com>
2020-09-06 00:52:21 -0300

    Move the files to the right place

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -1,39 +1,34 @@
 import styled from "styled-components";
 import { readableColor } from "polished";
 
-interface ButtonContainerProps {
-  variant?: "contained" | "outline" | "text";
-  color?: "primary" | "success" | "danger" | "warning" | "neutral";
-  size?: "sm" | "md" | "lg";
-  fullWidth?: boolean;
-}
-
-export const ButtonContainer = styled.button<ButtonContainerProps>`
+export const ButtonContainer = styled.button`
   background: ${(props) =>
-    props.variant === "outline" ? "transparent" : "red"};
+    props.variant === "outlined"
+      ? "transparent"
+      : props.theme.palette[props.color]};
   cursor: pointer;
   display: flex;
   align-items: center;
   justify-content: center;
   color: ${(props) =>
-    props.variant === "outline"
+    props.variant === "outlined"
       ? `${
           props.color
             ? props.theme.palette[props.color]
             : props.theme.palette.border
         }`
-      : readableColor("red" || "yellow")};
+      : readableColor(props.theme.palette[props.color] || "yellow")};
   border-radius: 4px;
   font-size: 0.875rem;
   text-align: center;
-  padding: ${(props) => (props.size === "sm" ? "8px" : "12px")};
+  padding: ${(props) => (props.size === "small" ? "8px" : "12px")};
   transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out,
     border-color 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
   border: none;
   outline: none;
   width: ${(props) => (props.fullWidth ? "100%" : "inherit")};
   border: ${(props) =>
-    props.variant === "outline"
+    props.variant === "outlined"
       ? `1px solid ${
           props.color
             ? props.theme.palette[props.color]
@@ -47,13 +42,33 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
 
   svg {
     color: ${(props) =>
-      props.variant === "outline"
+      props.variant === "outlined"
         ? `${
             props.color
               ? props.theme.palette[props.color]
               : props.theme.palette.border
           }`
-        : readableColor("red" || "yellow")};
+        : readableColor(props.theme.palette[props.color] || "yellow")};
     margin-right: 8px;
+  }
+`;
+
+export const IconButtonContainer = styled(ButtonContainer)`
+  background: ${(props) =>
+    props.color ? props.theme.palette[props.color] : "transparent"};
+  border-radius: 100%;
+  transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out,
+    border-color 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
+  padding: 8px;
+
+  &:hover {
+    background: rgba(0, 0, 0, 0.1);
+  }
+
+  svg {
+    height: 20px;
+    width: 20px;
+    margin: 0px;
+    color: ${(props) => props.theme.palette.text.secondary};
   }
 `;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,31 +2,19 @@ import React from "react";
 import { ButtonContainer } from "./Button.styles";
 
 export interface ButtonProps {
-  variant?: "contained" | "outline" | "text";
+  icon?: JSX.Element;
+  variant?: "outlined" | "contained" | "filled";
+  size?: "small" | "normal";
   type?: "button" | "submit";
-  color?: "primary" | "success" | "danger" | "warning" | "neutral";
-  size?: "sm" | "md" | "lg";
   fullWidth?: boolean;
+  onClick?: React.MouseEvent<HTMLButtonElement>;
+  color: "primary" | "secondary";
 }
 
-export const Button: React.FC<ButtonProps> = (props) => {
-  const {
-    children,
-    variant = "contained",
-    color = "neutral",
-    fullWidth,
-    size = "sm",
-    type = "button",
-  } = props;
-  return (
-    <ButtonContainer
-      type={type}
-      variant={variant}
-      color={color}
-      size={size}
-      fullWidth={fullWidth}
-    >
+export const Button: React.FC<ButtonProps> = React.forwardRef(
+  ({ children, type = "button", ...rest }, ref) => (
+    <ButtonContainer {...rest} ref={ref} type={type}>
       {children}
     </ButtonContainer>
-  );
-};
+  )
+);
