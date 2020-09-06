ad916e656d5ae80d903739a2351ed511255cb89a
Luan Tonin Galvan <ltoningalvan@gmail.com>
2020-09-05 21:57:07 -0300

    set up npm

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,21 +1,21 @@
-const path = require("path");
-const SRC_PATH = path.join(__dirname, "../src");
-const STORIES_PATH = path.join(__dirname, "../stories");
-//dont need stories path if you have your stories inside your //components folder
+const path = require("path")
+
 module.exports = ({ config }) => {
   config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    include: [SRC_PATH, STORIES_PATH],
+    test: /\.tsx?$/,
+    include: path.resolve(__dirname, "../src"),
     use: [
+      require.resolve("ts-loader"),
       {
-        loader: require.resolve("awesome-typescript-loader"),
+        loader: require.resolve("react-docgen-typescript-loader"),
         options: {
-          configFileName: "./.storybook/tsconfig.json",
-        },
-      },
-      { loader: require.resolve("react-docgen-typescript-loader") },
-    ],
-  });
-  config.resolve.extensions.push(".ts", ".tsx");
-  return config;
-};
+          tsconfigPath: path.resolve(__dirname, "../tsconfig.json"),
+        }
+      }
+    ]
+  })
+
+  config.resolve.extensions.push(".ts", ".tsx")
+
+  return config
+}
