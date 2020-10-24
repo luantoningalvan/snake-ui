60c98a975f4529d0ee87a6f31758930bc32334f5
Luan Tonin Galvan <ltoningalvan@gmail.com>
2020-10-24 13:31:19 -0300

    Cria componente Accordion

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { Card, CardHeader, CardBody, Collapse } from "../../";
+
+interface AccordionProps {
+  title: string;
+}
+
+export const Accordion: React.FC<AccordionProps> = ({
+  title,
+  children,
+  ...rest
+}) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(!open);
+
+  return (
+    <Card bordered {...rest}>
+      <CardHeader
+        title={title}
+        actions={[
+          {
+            action: handleOpen,
+            label: open ? <FiChevronUp /> : <FiChevronDown />,
+            type: "icon",
+            show: true,
+          },
+        ]}
+        onClick={handleOpen}
+        style={{ cursor: "pointer", padding: 16 }}
+      />
+
+      <Collapse expanded={open}>
+        <CardBody>{children}</CardBody>
+      </Collapse>
+    </Card>
+  );
+};
+
+export default Accordion;
